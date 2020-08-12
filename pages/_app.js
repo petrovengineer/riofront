fe9bee1712baf3010eb9eebf87dc5b7a76939674
Paul <petrovengineer@gmail.com>
2020-08-12 08:27:07 +0300

    fixpersist

--- pages/_app.js
+++ pages/_app.js
@@ -9,45 +9,52 @@ if (typeof window !== "undefined") {
 }
 
 function MyApp({ Component, pageProps }) {
-  const [customer, setCustomer] = useState(null);
-  const persist = (name, item, set)=>{
+  const [state, setStateSimple] = useState({
+    customer: null
+  });
+
+
+
+  const persist = (name)=>{
     return {
-      name: name,
-      get: item,
-      set:(item, newState)=>{
-        console.log(customer);
-        if(JSON.stringify(newState)!==localStorage.getItem(name)){
-          localStorage.setItem(name, JSON.stringify(newState))
+      get: state[name],
+      set:(value)=>{
+        console.log("SET", state);
+        if(JSON.stringify(value)!==localStorage.getItem(name)){
+          console.log("LOCAL", value)
+          localStorage.setItem(name, JSON.stringify(value))
         };
-        const persistState = Object.assign({}, item);
-        console.log("PERSIST SET", item);
-        persistState.get = newState;
-        set(persistState);
+        const newState = Object.assign({}, state);
+        console.log("NEW State", newState);
+        newState[name].get = value;
+        setStateSimple(newState);
       },
-      clear:(item)=>{
+      clear:()=>{
         localStorage.removeItem(name);
-        const persistState = Object.assign({}, item);
+        const persistState = Object.assign({}, customer);
         persistState.get = null;
         set(persistState);
       }
   }}
   
-  const restore = (item)=>{
-    console.log("RESTORE", item)
-      const local = localStorage.getItem(item.name);
-      if((item.get==null || item.get.length==0) && local!=null){
-        item.set(item, JSON.parse(local));
+  const restore = (name)=>{
+      console.log("RESTORE", state);
+      const local = localStorage.getItem(name);      
+      if((state[name].get==null || state[name].get.length==0) && local!=null){
+        console.log("RESTORE2 local", local);
+        state[name].set(JSON.parse(local));
       }
-      return null;
   }
   useEffect(()=>{
-    setCustomer(persist('customer', customer, setCustomer));
+    console.log("USEEFFECT")
+    state.customer = persist('customer');
+    restore('customer');
   }, [])
   useEffect(()=>{
-    console.log("EFFECT", customer);
-    if(customer!=null){restore(customer)}
-  }, [customer])
-  return <AppContext.Provider value={{customer}}>
+    console.log("CHANGE", state);
+  }, [state])
+
+  return <AppContext.Provider value={state}>
           <Component {...pageProps} />
         </AppContext.Provider>
 }
